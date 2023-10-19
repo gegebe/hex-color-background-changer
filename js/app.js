744820f143478659fab7b767c921d3ab6c871f4d
gegebe <ggirbes@gmail.com>
2023-10-19 11:29:40 +0200

    propuesta

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 // Implementar solución aquí
 
+function printBody(){
+
+  let printColour = generateHexColour();
+  let bodyColour;
+  let nameColour;
+
+  bodyColour = document.querySelector("body").style.backgroundColor = printColour;
+  nameColour = document.querySelector("#hex-value").textContent = printColour;
+
+  console.log(`El que genera la función: ${printColour}, el que pinta en el body ${bodyColour}, el que escribe en el span: ${nameColour}`);
+
+}
+
+function generateHexColour(){
+
+  let totalHexChar = "0123456789ABCDEF";
+  let randomHexColour = "#";
+  
+  for(let i = 0; i < 6; i++){
+  
+    randomHexColour += totalHexChar.charAt(Math.floor(Math.random() * totalHexChar.length));
+  
+  }
+  
+  return randomHexColour;
+}
